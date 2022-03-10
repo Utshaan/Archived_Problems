45be56e179b38d87e69a9467be25a16cc2c43d9b
Utshaan <Utkarsh.thanvi@gmail.com>
2022-03-10 18:31:59 +0530

    Fixed Problem_001.py

--- ArchiveddashProblems/Problem_001.py
+++ ArchiveddashProblems/Problem_001.py
@@ -1,17 +1,1 @@
-import math
-
-
-x = math.ceil(float(input("Enter the upper limit: ")))
-three = 0
-five = 0
-fifteen = 0
-for i in range(1,x+1):
-    if i%3==0:
-        three = three + i
-for i in range(1,x+1):
-    if i%5==0:
-        five = five + i
-for i in range(1,x+1):
-    if i%15==0:
-        fifteen = fifteen + i
-print("The sum is",three+five-fifteen)
+print(sum([i for i in range(1, 1000) if i % 3 == 0 or i % 5 == 0]))
