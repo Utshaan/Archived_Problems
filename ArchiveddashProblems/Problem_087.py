6f9783d29b13757f51de4df731cdd088f414d6f3
Utshaan <Utkarsh.thanvi@gmail.com>
2022-03-11 00:58:09 +0530

    Completed

---
+++ ArchiveddashProblems/Problem_087.py
@@ -0,0 +1,22 @@
+from fpack import primelist_till_x
+from time import time
+
+t = time()
+target = 50_000_000
+primes = sorted(list(primelist_till_x(int(target ** (1 / 2)))))
+num = set()
+
+for i in primes:
+    if i**4 > target:
+        break
+    else:
+        for j in primes:
+            if j**3 + i**4 > target:
+                break
+            else:
+                for k in primes:
+                    n = i**4 + j**3 + k**2
+                    if n < 50_000_000:
+                        num.add(n)
+
+print(f'Total Numbers = {len(num)},\nTime Taken: {time() - t} seconds')
