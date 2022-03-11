fd684eec1dce3bc3a4a32f4e257ec822202c6d63
Utshaan <Utkarsh.thanvi@gmail.com>
2022-03-11 20:06:53 +0530

    Completed

--- ArchiveddashProblems/Problem_093.py
+++ ArchiveddashProblems/Problem_093.py
@@ -1,9 +1,12 @@
+from time import time
 from itertools import combinations, product, permutations
 
+t1 = time()
 iterable = list(combinations(range(1, 10), r=4))
 
 iter_sym = tuple(product(["*", "+", "-", "/"], repeat=3))
 p = []
+dic = {}
 for k in iterable:
     o = list(permutations(k))
     l = set()
@@ -30,13 +33,16 @@ for k in iterable:
             l.add(int(do3)) if do3 > 0 and do3 == int(do3) else 10000
             l.add(int(do4)) if do4 > 0 and do4 == int(do4) else 10000
     p.append(tuple(sorted(l)))
+    dic[tuple(sorted(l))] = k
 
 high = 0
+k = 0
 for ele in p:
     start = 1
     while ele[start - 1] == start:
         start += 1
     if high < start:
         high = start
+        k = ele
 
-print(high)
+print(f'Answer: {sum(i * (10 ** (3 - index)) for index, i in enumerate(dic[k]))},\nTime Taken: {time() - t1}')
