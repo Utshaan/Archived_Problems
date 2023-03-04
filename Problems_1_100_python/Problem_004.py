983bb101270311d3a3c78fbcffd66a5ac18b38dc
TrianglEle <123717770+TrianglEle@users.noreply.github.com>
2023-03-04 15:17:36 +0530

    Update Problem_004.py

--- Problems_1_100_python/Problem_004.py
+++ Problems_1_100_python/Problem_004.py
@@ -2,11 +2,11 @@ import time
 
 start = time.time()
 answers = []
-for i in range(1000, 100, -1):
-    for j in range(1000, 100, -1):
+for i in range(999, 99, -1):
+    for j in range(999, 99, -1):
         temp = str(i*j)
-        if temp == temp[::-1]
-        answers.append(temp)
-        break
+        if temp == temp[::-1]:
+            answers.append(i*j)
+            break
 
 print(max(answers), time.time() - start)
